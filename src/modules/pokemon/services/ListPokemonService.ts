3326164cd4289a4bf1fb8a9824f68e9ecd08a727
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-27 12:47:41 -0300

    Creating pokemon list all from db service

---
+++ src/modules/pokemon/services/ListPokemonService.ts
@@ -0,0 +1,16 @@
+import { getCustomRepository } from "typeorm";
+import { PokemonRepository } from "../typeorm/repositories/PokemonRepository";
+import AppError from "@shared/errors/AppError";
+import PokemonEntitie from "../typeorm/entities/pokemonEntitie";
+
+
+class ListPokemonService {
+    public async execute(): Promise<PokemonEntitie[]> {
+        const pokemonRepository = getCustomRepository(PokemonRepository);
+        const pokemon = await pokemonRepository.find();//getting all pokemon instances from db
+
+        return pokemon;
+    }
+}
+
+export default ListPokemonService;
