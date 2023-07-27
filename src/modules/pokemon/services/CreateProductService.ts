5108797f0ff9a66034e64eab4ae9da718828cadf
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-27 12:41:47 -0300

    Pokémon create service to persist pokemon on db

---
+++ src/modules/pokemon/services/CreateProductService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from "typeorm";
+import { PokemonRepository } from "../typeorm/repositories/PokemonRepository";
+import AppError from "@shared/errors/AppError";
+import PokemonEntitie from "../typeorm/entities/pokemonEntitie";
+
+interface IRequest {
+    name: string;
+    stage_0_name: string;
+    stage_1_name: string;
+    stage_1_lvlup: number;
+    stage_2_name: string;
+    stage_2_lvlup: number;
+    range: string;
+    role: string;
+    difficulty: string;
+}
+
+class CreateProductService {
+    public async execute({ name, stage_0_name, stage_1_name, stage_1_lvlup, stage_2_name, stage_2_lvlup, range, role, difficulty }: IRequest): Promise<PokemonEntitie> {
+        const pokemonRepository = getCustomRepository(PokemonRepository);
+        let pokemon = await pokemonRepository.findByName(name);
+
+        if (pokemon) throw new AppError('Pokemon already exists');
+
+        pokemon = pokemonRepository.create({
+            name, stage_0_name, stage_1_name, stage_1_lvlup, stage_2_name, stage_2_lvlup, range, role, difficulty
+        }); //Preparing object to save on DB
+
+        await pokemonRepository.save(pokemon);
+
+        return pokemon;
+    }
+}
+
+export default CreateProductService;
