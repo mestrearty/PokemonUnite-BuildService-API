7f655d39c3a60eaf02c7de46edc01182c0d45444
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-27 12:20:49 -0300

    Creating Pokemon Entitie and Repository

---
+++ src/modules/pokemon/typeorm/entities/pokemonEntitie.ts
@@ -0,0 +1,42 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('pokemon')
+class PokemonEntitie {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    stage_0_name: string;
+
+    @Column()
+    stage_1_name: string;
+
+    @Column('int')
+    stage_1_lvlup: number;
+
+    @Column()
+    stage_2_name: string;
+
+    @Column('int')
+    stage_2_lvlup: number;
+
+    @Column()
+    range: string;
+
+    @Column()
+    role: string;
+
+    @Column()
+    difficulty: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default PokemonEntitie;
---
+++ src/modules/pokemon/typeorm/repositories/pokemonRepository.ts
@@ -0,0 +1,15 @@
+import { EntityRepository, Repository } from "typeorm";
+import PokemonEntitie from "../entities/pokemonEntitie";
+
+@EntityRepository(PokemonEntitie)
+export class ProductsRepository extends Repository<PokemonEntitie> {
+    public async findByName(name: string): Promise<PokemonEntitie | undefined> {
+        const product = this.findOne({
+            where: {
+                name
+            }
+        });
+
+        return product;
+    }
+}
